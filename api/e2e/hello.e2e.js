960532d617c47dfb0f8a454c41a5a6a3a021c23e
unknown <lorenzopenalver3@gmail.com>
2024-01-10 11:06:46 -0300

    Configurando supertest en el API

---
+++ api/e2e/hello.e2e.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+
+const createApp = require('../src/app.js')
+
+describe('Test for Hello Endpoint', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+  afterAll(async () => {
+    await server.close()
+  });
+  describe('Test for GET', () => {
+    test('Should return "Hello World!"', () => request(app)
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('Hello World!')
+      }));
+  });
+});
